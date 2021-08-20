45143d07ec99cae35bdd20169094fbee2b1c203e
DESKTOP-HL4JQ36\ADMN <liberattamunene@gmail.com>
2021-08-20 10:31:36 +0300

    Changes To Assignment1

--- instat/dlgMyFirstDialog.Designer.vb
+++ instat/dlgMyFirstDialog.Designer.vb
@@ -24,14 +24,17 @@ Partial Class dlgBarChartDesign
     Private Sub InitializeComponent()
         Me.ucrBase = New instat.ucrButtons()
         Me.ucrColors = New instat.ucrColors()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.ucrDataFrame = New instat.ucrDataFrame()
-        Me.ucrDataSelectionVariables = New instat.ucrDataSelection()
         Me.lblColour = New System.Windows.Forms.Label()
         Me.rdoSimple = New instat.ucrRadio()
         Me.rdoClustered = New instat.ucrRadio()
         Me.rdoStacked = New instat.ucrRadio()
         Me.grpBarChartType = New System.Windows.Forms.GroupBox()
+        Me.ucrSelectorDataFrame = New instat.ucrSelectorByDataFrameAddRemove()
+        Me.ucrReceiverX = New instat.ucrReceiverSingle()
+        Me.ucrReceiverY = New instat.ucrReceiverSingle()
+        Me.lblXVariable = New System.Windows.Forms.Label()
+        Me.lblYVariable = New System.Windows.Forms.Label()
+        Me.ucrSaveGraph = New instat.ucrSaveGraph()
         Me.grpBarChartType.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -48,41 +51,15 @@ Partial Class dlgBarChartDesign
         Me.ucrColors.AddQuotesIfUnrecognised = True
         Me.ucrColors.GetSetSelectedIndex = -1
         Me.ucrColors.IsReadOnly = False
-        Me.ucrColors.Location = New System.Drawing.Point(203, 223)
+        Me.ucrColors.Location = New System.Drawing.Point(264, 201)
         Me.ucrColors.Name = "ucrColors"
         Me.ucrColors.Size = New System.Drawing.Size(137, 21)
         Me.ucrColors.TabIndex = 6
         '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(263, 141)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 7
-        Me.Label1.Text = "Label1"
-        '
-        'ucrDataFrame
-        '
-        Me.ucrDataFrame.bDropUnusedFilterLevels = False
-        Me.ucrDataFrame.bUseCurrentFilter = True
-        Me.ucrDataFrame.Location = New System.Drawing.Point(12, 9)
-        Me.ucrDataFrame.Margin = New System.Windows.Forms.Padding(0)
-        Me.ucrDataFrame.Name = "ucrDataFrame"
-        Me.ucrDataFrame.Size = New System.Drawing.Size(148, 50)
-        Me.ucrDataFrame.TabIndex = 8
-        '
-        'ucrDataSelectionVariables
-        '
-        Me.ucrDataSelectionVariables.Location = New System.Drawing.Point(12, 55)
-        Me.ucrDataSelectionVariables.Name = "ucrDataSelectionVariables"
-        Me.ucrDataSelectionVariables.Size = New System.Drawing.Size(285, 122)
-        Me.ucrDataSelectionVariables.TabIndex = 9
-        '
         'lblColour
         '
         Me.lblColour.AutoSize = True
-        Me.lblColour.Location = New System.Drawing.Point(200, 199)
+        Me.lblColour.Location = New System.Drawing.Point(261, 182)
         Me.lblColour.Name = "lblColour"
         Me.lblColour.Size = New System.Drawing.Size(37, 13)
         Me.lblColour.TabIndex = 10
@@ -121,16 +98,79 @@ Partial Class dlgBarChartDesign
         Me.grpBarChartType.TabStop = False
         Me.grpBarChartType.Text = "Bar Chart Design "
         '
+        'ucrSelectorDataFrame
+        '
+        Me.ucrSelectorDataFrame.bDropUnusedFilterLevels = False
+        Me.ucrSelectorDataFrame.bShowHiddenColumns = False
+        Me.ucrSelectorDataFrame.bUseCurrentFilter = True
+        Me.ucrSelectorDataFrame.Location = New System.Drawing.Point(12, -1)
+        Me.ucrSelectorDataFrame.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrSelectorDataFrame.Name = "ucrSelectorDataFrame"
+        Me.ucrSelectorDataFrame.Size = New System.Drawing.Size(210, 180)
+        Me.ucrSelectorDataFrame.TabIndex = 15
+        '
+        'ucrReceiverX
+        '
+        Me.ucrReceiverX.frmParent = Me
+        Me.ucrReceiverX.Location = New System.Drawing.Point(264, 48)
+        Me.ucrReceiverX.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverX.Name = "ucrReceiverX"
+        Me.ucrReceiverX.Selector = Nothing
+        Me.ucrReceiverX.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverX.strNcFilePath = ""
+        Me.ucrReceiverX.TabIndex = 16
+        Me.ucrReceiverX.ucrSelector = Nothing
+        '
+        'ucrReceiverY
+        '
+        Me.ucrReceiverY.frmParent = Me
+        Me.ucrReceiverY.Location = New System.Drawing.Point(264, 111)
+        Me.ucrReceiverY.Margin = New System.Windows.Forms.Padding(0)
+        Me.ucrReceiverY.Name = "ucrReceiverY"
+        Me.ucrReceiverY.Selector = Nothing
+        Me.ucrReceiverY.Size = New System.Drawing.Size(120, 20)
+        Me.ucrReceiverY.strNcFilePath = ""
+        Me.ucrReceiverY.TabIndex = 17
+        Me.ucrReceiverY.ucrSelector = Nothing
+        '
+        'lblXVariable
+        '
+        Me.lblXVariable.AutoSize = True
+        Me.lblXVariable.Location = New System.Drawing.Point(261, 35)
+        Me.lblXVariable.Name = "lblXVariable"
+        Me.lblXVariable.Size = New System.Drawing.Size(55, 13)
+        Me.lblXVariable.TabIndex = 18
+        Me.lblXVariable.Text = "X Variable"
+        '
+        'lblYVariable
+        '
+        Me.lblYVariable.AutoSize = True
+        Me.lblYVariable.Location = New System.Drawing.Point(261, 98)
+        Me.lblYVariable.Name = "lblYVariable"
+        Me.lblYVariable.Size = New System.Drawing.Size(55, 13)
+        Me.lblYVariable.TabIndex = 19
+        Me.lblYVariable.Text = "Y Variable"
+        '
+        'ucrSaveGraph
+        '
+        Me.ucrSaveGraph.Location = New System.Drawing.Point(264, 237)
+        Me.ucrSaveGraph.Name = "ucrSaveGraph"
+        Me.ucrSaveGraph.Size = New System.Drawing.Size(313, 32)
+        Me.ucrSaveGraph.TabIndex = 20
+        '
         'dlgBarChartDesign
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(423, 353)
+        Me.Controls.Add(Me.ucrSaveGraph)
+        Me.Controls.Add(Me.lblYVariable)
+        Me.Controls.Add(Me.lblXVariable)
+        Me.Controls.Add(Me.ucrReceiverY)
+        Me.Controls.Add(Me.ucrReceiverX)
+        Me.Controls.Add(Me.ucrSelectorDataFrame)
         Me.Controls.Add(Me.grpBarChartType)
         Me.Controls.Add(Me.lblColour)
-        Me.Controls.Add(Me.ucrDataSelectionVariables)
-        Me.Controls.Add(Me.ucrDataFrame)
-        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ucrColors)
         Me.Controls.Add(Me.ucrBase)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
@@ -147,12 +187,15 @@ Partial Class dlgBarChartDesign
 
     Friend WithEvents ucrBase As ucrButtons
     Friend WithEvents ucrColors As ucrColors
-    Friend WithEvents Label1 As Label
-    Friend WithEvents ucrDataFrame As ucrDataFrame
-    Friend WithEvents ucrDataSelectionVariables As ucrDataSelection
     Friend WithEvents lblColour As Label
     Friend WithEvents rdoSimple As ucrRadio
     Friend WithEvents rdoClustered As ucrRadio
     Friend WithEvents rdoStacked As ucrRadio
     Friend WithEvents grpBarChartType As GroupBox
+    Friend WithEvents ucrSelectorDataFrame As ucrSelectorByDataFrameAddRemove
+    Friend WithEvents ucrReceiverX As ucrReceiverSingle
+    Friend WithEvents lblXVariable As Label
+    Friend WithEvents ucrReceiverY As ucrReceiverSingle
+    Friend WithEvents lblYVariable As Label
+    Friend WithEvents ucrSaveGraph As ucrSaveGraph
 End Class
